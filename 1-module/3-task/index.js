86548cd3bda6b9c17601736c227fd985b4efac5e
ihnat els <alexander.k@elastic-soft.com>
2024-02-24 19:43:27 +0400

    first uppercase

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,6 @@
 function ucFirst(str) {
-  // ваш код...
+  if (!str.length)  return str
+  if (str.length === 1)  return str.toUpperCase()
+  const firstSymbol = str[0].toUpperCase()
+  return firstSymbol + str.slice(1)
 }
