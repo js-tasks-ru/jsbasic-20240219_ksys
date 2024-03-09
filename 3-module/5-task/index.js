143d4b5466c68fa056d19cffecffc5237e024674
Alexandr <bycallla@ya.ru>
2024-03-09 19:35:40 +0400

    min max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbersArray = str.split(' ').filter(i => !isNaN(i))
+  
+  return {
+    min: Math.min(...numbersArray),
+    max: Math.max(...numbersArray),
+  }
 }
