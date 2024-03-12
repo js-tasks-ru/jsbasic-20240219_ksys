123fb4413d0a7c3f6ff83dd7fdec588e7b44fa6b
Alexandr <bycallla@ya.ru>
2024-03-12 23:07:08 +0400

    DOM

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,28 +1,11 @@
-let friends = [
-  {
-      firstName: 'Artsiom',
-      lastName: 'Mezin'
-  },
-  {
-      firstName: 'Ilia',
-      lastName: 'Kantor'
-  },
-  {
-      firstName: 'Christopher',
-      lastName: 'Michael'
-  }
-];
 function makeFriendsList(friends) {
-
   let elUl = document.createElement('ul')
-  console.log('elUl1',elUl)
+  
   friends.map(i => {
-    let elLi = document.createElement('li')
-    // elLi.innerHtml(`i.firstName + ' ' + i.lastName`)
+    let elLi = document.createElement('li')    
     elLi.appendChild(document.createTextNode(i.firstName + ' ' + i.lastName))
     elUl.appendChild(elLi)
   })
   return elUl
 }
 makeFriendsList(friends)
-
