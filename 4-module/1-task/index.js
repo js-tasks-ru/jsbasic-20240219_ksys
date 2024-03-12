178084b20cb572ace9095b2d562ed5a581c4c5ed
Alexandr <bycallla@ya.ru>
2024-03-12 22:01:06 +0400

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,28 @@
+let friends = [
+  {
+      firstName: 'Artsiom',
+      lastName: 'Mezin'
+  },
+  {
+      firstName: 'Ilia',
+      lastName: 'Kantor'
+  },
+  {
+      firstName: 'Christopher',
+      lastName: 'Michael'
+  }
+];
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let elUl = document.createElement('ul')
+  console.log('elUl1',elUl)
+  friends.map(i => {
+    let elLi = document.createElement('li')
+    // elLi.innerHtml(`i.firstName + ' ' + i.lastName`)
+    elLi.appendChild(document.createTextNode(i.firstName + ' ' + i.lastName))
+    elUl.appendChild(elLi)
+  })
+  return elUl
 }
+makeFriendsList(friends)
+
