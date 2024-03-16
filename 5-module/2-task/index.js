b4e6edcf1e54305cc960c334996459a74634d5fc
Alexandr <bycallla@ya.ru>
2024-03-16 22:54:29 +0400

    hide text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,4 @@
 function toggleText() {
-  // ваш код...
+  let hiddenButton = document.querySelector('.toggle-text-button')
+  hiddenButton.addEventListener('click', () => {text.hidden === true ? text.hidden = false : text.hidden = true})
 }
