49199b504498aa470873cae7abfa811a45849fba
Alexandr <bycallla@ya.ru>
2024-03-17 09:40:58 +0400

    carousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,15 @@
 function initCarousel() {
-  // ваш код...
+  let slideIndex = 0  
+  const rightArrow = document.querySelector('.carousel__arrow_right')
+  const leftArrow = document.querySelector('.carousel__arrow_left')
+  leftArrow.style.display = 'none'
+  rightArrow.addEventListener('click', () => {slideIndex++; render()})
+  leftArrow.addEventListener('click', () => {slideIndex--; render()})
+  
+  function render() {   
+    slideIndex === 0 ? leftArrow.style.display = 'none' : leftArrow.style.display = '';
+    slideIndex === 3 ? rightArrow.style.display = 'none' : rightArrow.style.display = '';
+    let offset = document.querySelector('.carousel__inner').offsetWidth    
+    document.querySelector('.carousel__inner').style.transform = `translateX(${-slideIndex * offset}px)`    
+  }  
 }
